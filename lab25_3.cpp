eef2e0cc5dadc7f88a6976083cbbf99085442657
pinnaree <pprommitr@gmail.com>
2023-03-13 00:40:18 +0700

    lab25-3 completed

--- lab25_3.cpp
+++ lab25_3.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include<list>
 
 using namespace std;
 
-//Write function count() here.
+int count(int data[],int size){
+	list<int> line_up;
+	list<int>::iterator loc;
+	for(int i = 0; i < size; i++){
+			line_up.push_back(data[i]);
+	}
+	line_up.sort();
+	line_up.unique();
+	return line_up.size();
+
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
